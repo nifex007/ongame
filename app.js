8899557fd64d85e73c775ab384ec59cdd183393c
Nifemi O. Sola-Ojo <nifex007@yahoo.com>
2016-11-03 17:51:02 +0100

    resolving paths and more on implementing node in the project

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-var express = require ("express");
+var express = require ('express');
 var app = express ();
+var path = require('path');
 
-app.get('/', function(req, res){
-	res.send('./landing/landing.html');
-});
+ ///////try & error codes
+//app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'html');
+
+/*app.get('/', function(req, res){
+	res.render('./landing/landing.html');
+}); 
 
 app.listen(8080, function(){
 	console.log('Listening on port 8080');
 });
+*/
+
+//path resolution
+app.use(express.static(__dirname + '/pages'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/landing'));
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/blog'));
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/landing/landing.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+/*app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+*/
+app.listen(8080);
+
+console.log("Running at Port 8080");
